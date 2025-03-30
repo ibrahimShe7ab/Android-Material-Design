cc80fe56791091cb5b518879ebbb9efdb6e981c1
shehab <ibrahimshehab2034@gmail.com>
2025-03-30 11:48:46 +0200

    card view implementation

--- app/src/main/java/com/example/androidmaterialdesign/MainActivity.kt
+++ app/src/main/java/com/example/androidmaterialdesign/MainActivity.kt
@@ -6,8 +6,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.example.androidmaterialdesign.databinding.ActivityMainBinding
-import com.example.androidmaterialdesign.databinding.CardViewBinding
+ import com.example.androidmaterialdesign.databinding.CardViewBinding
 
 class MainActivity : AppCompatActivity() {
 //    lateinit var binding:ActivityMainBinding
