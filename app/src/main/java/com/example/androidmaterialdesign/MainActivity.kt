6d1e6eb4cfb22f8dc3d21195cdfe282c7ec285d7
shehab <ibrahimshehab2034@gmail.com>
2025-03-30 11:47:09 +0200

    card view implementation

--- app/src/main/java/com/example/androidmaterialdesign/MainActivity.kt
+++ app/src/main/java/com/example/androidmaterialdesign/MainActivity.kt
@@ -7,16 +7,25 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.androidmaterialdesign.databinding.ActivityMainBinding
+import com.example.androidmaterialdesign.databinding.CardViewBinding
 
 class MainActivity : AppCompatActivity() {
-    lateinit var binding:ActivityMainBinding
+//    lateinit var binding:ActivityMainBinding
+    lateinit var binding: CardViewBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding=ActivityMainBinding.inflate(layoutInflater)
+//        binding=ActivityMainBinding.inflate(layoutInflater)
+        binding= CardViewBinding.inflate(layoutInflater)
         setContentView(binding.root)
-binding.fab.setOnClickListener { Toast.makeText(this, "fab clicked", Toast.LENGTH_SHORT).show() }
+//        callFab()
+
+
     }
 
+    fun callFab(){
+//binding.fab.setOnClickListener { Toast.makeText(this, "fab clicked", Toast.LENGTH_SHORT).show() }
 
+
+    }
 
 }
