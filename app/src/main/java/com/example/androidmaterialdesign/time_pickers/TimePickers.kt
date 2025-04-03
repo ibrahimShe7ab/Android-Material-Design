db2d2741c1d6ddb7dc6ac34bdf6803ce515e167f
shehab <ibrahimshehab2034@gmail.com>
2025-04-03 15:03:07 +0200

    Material-picker-time

---
+++ app/src/main/java/com/example/androidmaterialdesign/time_pickers/TimePickers.kt
@@ -0,0 +1,66 @@
+package com.example.androidmaterialdesign.time_pickers
+
+import android.os.Bundle
+import android.text.format.DateFormat.is24HourFormat
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.androidmaterialdesign.R
+import com.example.androidmaterialdesign.databinding.ActivityTimePickersBinding
+import com.google.android.material.timepicker.MaterialTimePicker
+import com.google.android.material.timepicker.TimeFormat
+
+class TimePickers : AppCompatActivity() {
+    lateinit var binding: ActivityTimePickersBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding= ActivityTimePickersBinding.inflate(layoutInflater)
+         setContentView(binding.root)
+
+
+        callback()
+     }
+
+    private fun callback() {
+        binding.picker.setOnClickListener { timePicker() }
+    }
+
+
+    private fun timePicker() {
+             val isSystem24Hour = is24HourFormat(this)
+            val clockFormat = if (isSystem24Hour) TimeFormat.CLOCK_24H else TimeFormat.CLOCK_12H
+
+             val picker = MaterialTimePicker.Builder()
+                .setTimeFormat(clockFormat)
+                .setHour(4)
+                .setMinute(44)
+                 .setTheme(R.style.Theme_App)
+                  .setTitleText("Select Appointment time")
+                .build()
+
+             picker.show(supportFragmentManager, "timePicker")
+
+             picker.addOnPositiveButtonClickListener {
+                val selectedHour = picker.hour
+                val selectedMinute = picker.minute
+                 Toast.makeText(this, "الوقت المختار: $selectedHour:$selectedMinute", Toast.LENGTH_SHORT).show()
+            }
+        picker.addOnNegativeButtonClickListener {
+            Toast.makeText(this, "time not selected", Toast.LENGTH_SHORT).show()
+        }
+        picker.addOnCancelListener {
+            Toast.makeText(this, "time selected cancel", Toast.LENGTH_SHORT).show()
+        }
+        picker.addOnDismissListener {
+            Toast.makeText(this, "finish", Toast.LENGTH_SHORT).show()        }
+
+
+
+        }
+
+    }
+
+
+
